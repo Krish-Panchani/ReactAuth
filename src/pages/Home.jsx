aca85875e0bc6b8c27f28c2af499b8dcf93928e8
Krish-Panchani <krishpanchani1346@gmail.com>
2025-01-20 18:38:59 +0530

    Add Home, Login, and Register page components

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Todo from '../components/todo';
+export default function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+            <Todo />
+        </div>
+    );
+}
